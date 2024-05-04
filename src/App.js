a60b2a940c1e9a4dc5b25977e0164ac923747ae0
elavarasan03e <elu03.e@gmail.com>
2024-05-04 17:39:13 +0530

    search Countries react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [country, setCountry] = useState([]);
+  const [searchCountry,setSearchCountry] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        fetch("https://restcountries.com/v3.1/all")
+        .then(res=>res.json())
+        .then(data=>setCountry(data));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleChange=(e)=>{
+    setSearchCountry(e.target.value);
+    console.log(searchCountry);
+  }
+
+  const filteredCountries = country.filter(country => {
+    return country.name.common.toLowerCase().includes(searchCountry.toLowerCase());
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="search"> 
+      <input type='text' placeholder="Search for Countries" value={searchCountry} onChange={handleChange} />
+      </div>
+
+      <div className="country-container">
+      
+      {filteredCountries.map((country) => {
+          return (
+            <div key={country.cca3} className="countryCard">
+              <img src={country.flags.png} alt={`Flag of ${country.name.common}`} />
+              <h2>{country.name.common}</h2>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
-
-export default App;
